ddb70931764f7334a087c0c5ccbb167f7fd85e4c
Yomaira Escano <yjescano@gmail.com>
2018-09-04 03:05:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const produceDrivingRange = function(trip){
-  return function(blockRange1, blockRange2){
-    if (trip === blockRange2 - blockRange1) {
-      return '2 block out of range'
-    }
+function produceDrivingRange (blockRange) {
+  return function() {
+    
   }
 }
