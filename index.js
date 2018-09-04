2864d5246aca3dcb557f2a84e12a03acb909a742
Yomaira Escano <yjescano@gmail.com>
2018-09-04 03:37:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function produceDrivingRange(blockRange) {
+  return function(start, end) {
+
+    let startBlock = parseInt(start)
+    let endBlock = parseInt(end)
+    let distanceTravelled = Math.abs(endBlock - startBlock)
+    let rangeDifference = blockRange - distanceTravelled
+
+    if(rangeDifference > 0) {
+      return `within range by ${rangeDifference}`
+    } else {
+      return `${Math.abs(rangeDifference)} blocks out of range`
+    }
+  }
+}
+
+function produceTipCalculator(tipPercentage) {
+  return function(amount) {
+    return tipPercentage * amount
+  }
+}
+
+function createDriver(){
+  let driverId = 0
+
+  return class {
+    constructor(name){
+      this.name = name
+      this.id = ++driverId
+    }
+  }
+}
