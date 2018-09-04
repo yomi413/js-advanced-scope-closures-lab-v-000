bf1cf5eda8c6fed0765889b46b976bd7db3f123f
Yomaira Escano <yjescano@gmail.com>
2018-09-04 03:18:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function produceDrivingRange (blockRange) {
-  return function() {
-    
+  return function(startingBlock, endingBlock) {
+    let start = Math.parseInt(startingBlock)
+    let end = Math.parseInt(endingBlock)
+    let travelledDistance = Math.abs(end - start)
+
+    if(blockRange - travelledDistance > 0) {
+      return `within range by ${blockRange - travelledDistance}`
+    }
   }
 }
